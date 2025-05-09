db22366696201667e59069710cc4a06a211c2585
Ishan Gupta <ishan.gupta@talentmonk.com>
2025-05-09 19:14:02 +0000

    a

--- src/modules/leave/manager-leave-list/index.ts
+++ src/modules/leave/manager-leave-list/index.ts
@@ -4,31 +4,51 @@ import { createHTTPResponse } from '@talent-monk/utils';
 import { StatusCodes } from 'http-status-codes';
 import timePrisma from 'utils/prisma/time-tracker';
 import { EmployeeHandler } from '@talent-monk/client-shared';
+import prismaFactory from 'utils/prisma';
 
 export const managerLeaveListEndpoint = createPrivateEndpointWithZod(
-  'MANAGER LEAVE LIST',
-  z.object({
-  }),
-  z.any(),
-  async (input) => {
-    try {
-        const prisma = new Pri
-      const employeesUnderMe = await prisma.employeeFieldData.findMany({
-        where: {
-            value: user.id,
-            field: {
-                name: 'reportingManager'
-            }
-        }
-    })
+    'MANAGER LEAVE LIST',
+    z.object({
+    }),
+    z.any(),
+    async (input) => {
+        try {
+            const prisma = prismaFactory.get();
+            const currentUser = await prisma.user.findUnique({
+                where: {
+                    uid: input.userAuthId
+                },
+                select: {
+                    id: true
+                }
+            })
+            const employeesUnderMe = await prisma.employeeFieldData.findMany({
+                where: {
+                    value: currentUser?.id,
+                    field: {
+                        name: 'reportingManager'
+                    }
+                },
+                select: {
+                    userId: true
+                }
+            })
 
-    const empIds = empData.map((emp) => emp.userId);;
-    } catch (error) {
-      console.error('Leave policy upsert error:', error);
-      throw new Error('Failed to upsert leave policy');
+            const empIds = employeesUnderMe.map((emp: { userId: string }) => emp.userId);
+
+            const leaves = await prisma.leave.findMany({
+                where: {
+                    uid: {
+                        in: empIds
+                    }
+                }
+            })
+        } catch (error) {
+            console.error('Leave policy upsert error:', error);
+            throw new Error('Failed to upsert leave policy');
+        }
+    },
+    async (res, output) => {
+        createHTTPResponse(res, StatusCodes.OK, output.message, output);
     }
-  },
-  async (res, output) => {
-    createHTTPResponse(res, StatusCodes.OK, output.message, output);
-  }
 );
