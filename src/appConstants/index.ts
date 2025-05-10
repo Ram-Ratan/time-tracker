e966232299b5dcd25089ad8891888bc32ce5503d
Ram Singh <ram.singh@talentmonk.com>
2025-05-10 13:22:32 +0530

    version up utils to avoid cors

--- src/appConstants/index.ts
+++ src/appConstants/index.ts
@@ -9,13 +9,7 @@ export const appConstants = parseEnv(
         SURFACE_SERVICE_URL: z.string().url(),
         MONK_SERVICE_URL: z.string().url(),
         COMMUNICATIONS_SERVICE_URL: z.string().url(),
-        LIKE_MAIL_TEMPLATE: z.string(),
-        COMMENT_MAIL_TEMPLATE: z.string(),
         WORKFLOW_SERVICE_URL: z.string().url(),
         STORAGE_CONTAINER: z.string(),
-        LINKEDIN_CLIENTID: z.string(),
-        LINKEDIN_CLIENTSECRET: z.string(),
-        LINKEDIN_URL: z.string().url(),
-        LINKEDIN_ACCESS_TOKEN_URL: z.string().url()
     })
 );
