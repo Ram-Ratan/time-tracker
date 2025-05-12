11ca8e335721babfee015213fe6364c12277be61
Ram Singh <ram.singh@talentmonk.com>
2025-05-12 15:42:52 +0530

    last commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import leaveRouter from 'modules/leave/leaveRouter';
 import organizationRouter from 'modules/organization/organizationRouter';
 import userRouter from 'modules/user/userRouter';
 import openAIRouter from 'modules/openAi/openAIRouter';
+import timeTrackerServiceRouter from 'modules/time-tracker/timeTracker.router';
 
 const surfaceServiceRouter = Router();
 
@@ -16,5 +17,6 @@ surfaceServiceRouter.use('/leave', leaveRouter);
 surfaceServiceRouter.use('/organization', organizationRouter);
 surfaceServiceRouter.use('/user', userRouter);
 surfaceServiceRouter.use('/openai', openAIRouter);
+surfaceServiceRouter.use('/time-tracker', timeTrackerServiceRouter);
 
 export default surfaceServiceRouter;
